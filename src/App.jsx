3878776d3a0d6124f65f15c1c5aa98a9759a877a
unknown <a.cina@procreatech.com>
2025-03-12 13:42:16 +0100

    JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css';
 
-// JSX: sintassi con i tag, non è né una stringa né HTML.
-// È chiamata JSX, ed è un’estensione della sintassi JavaScript. Utilizzata con React per descrivere l’aspetto che dovrebbe avere la UI (User Interface, o interfaccia utente). JSX potrebbe ricordare un linguaggio di template, ma usufruisce di tutta la potenza del JavaScript.
-// JSX produce “elementi React”.
+// un componente in react è una funzione js che ritorna codice jsx
+// il nome di un componente inizia sempre con una lettera maiuscola
+// il codice ritornato deve essere sempre contenuto in un parent html (one root element), ad esempio posso ritornare un <div><div/> con dentro altro codice, ma se volessi ritornare più elementi html dello stesso livello (ad esempio 2 div siblings) avrei un errore, per non averlo posso utilizzare un react fragment, cioè wrappo tutto tra <> codice </> (è un empty html tag)
 function App() {
   return (
     <>
-      <div></div>
+      <div>
+        <p>hello world</p>
+      </div>
+      <div>
+        <p>hello world 2</p>
+      </div>
     </>
   );
 }
